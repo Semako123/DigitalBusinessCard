ba8fc4c708a8eb76d49d14a56a3d7b50547fed17
semako123 <zosusemenoch@gmail.com>
2022-08-05 05:45:26 +0100

    Set up and integrate the about component

--- my-business-card/src/App.js
+++ my-business-card/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import React from 'react';
 import Info from './components/info/info';
+import About from './components/about/about';
 
 function App() {
 	return (
 		<div className='app'>
 			<Info />
+			<About />
 		</div>
 	);
 }
---
+++ my-business-card/src/components/about/about.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import './about.css';
+
+class About extends Component {
+	render() {
+		return (
+			<div className='main'>
+				<h3>About</h3>
+				<p>
+					I am a front-end developer who is passionate about his work.
+					Experienced with working with HTML, CSS, Javascript, Python (Flask)
+					and React. Let’s make something great.
+				</p>
+				<h3>Interests</h3>
+				<ul>
+					<li>Robotics and Embedded designs</li>
+					<li>Arduino and microcontrollers</li>
+					<li>Tourism and camping</li>
+					<li>Gaming</li>
+				</ul>
+			</div>
+		);
+	}
+}
+
+export default About;
