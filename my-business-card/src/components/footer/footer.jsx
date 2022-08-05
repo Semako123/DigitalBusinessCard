518aeb3b6318114ea11eeedb0f939c8bb88ec4eb
semako123 <zosusemenoch@gmail.com>
2022-08-05 11:31:35 +0100

    Add the footer logos Embeds the footer with links Restyled the info component buttons

--- my-business-card/src/components/footer/footer.jsx
+++ my-business-card/src/components/footer/footer.jsx
@@ -1,17 +1,44 @@
 import React, { Component } from 'react';
-import './footer.css'
+import './footer.css';
+import lin from './icons8-linkedin.svg';
+import github from './icons8-github.svg';
+import twitter from './icons8-twitter-24.svg';
+import whatsapp from './icons8-whatsapp-24.svg';
 
-class Footer extends Component { 
-    render() { 
-        return (
-            <div className='footer'>
-                <div className="footer-icon"></div>
-                <div className="footer-icon"></div>
-                <div className="footer-icon"></div>
-                <div className="footer-icon"></div>
-            </div>
-        );
-    }
+class Footer extends Component {
+	render() {
+		return (
+			<div className='footer'>
+				<a href='https://www.linkedin.com/in/semako-zosu'>
+					<div className='footer-icon'>
+						<img src={lin} alt='logo of linkedIn ' />
+					</div>
+				</a>
+				<a href='https://github.com/Semako123'>
+					<div className='footer-icon'>
+						<img src={github} alt='logo of github ' />
+					</div>
+				</a>
+				<a
+					class='twitter-timeline'
+					href='https://twitter.com/Semako64784400?ref_src=twsrc%5Etfw'>
+					<div className='footer-icon'>
+						<img src={twitter} alt='logo of twitter ' />
+					</div>
+				</a>
+                <a href='https://wa.me/7044746657'>
+					<div className='footer-icon'>
+						<img src={whatsapp} alt='logo of whatsapp ' />
+					</div>
+				</a>
+				<script
+					async
+					src='https://platform.twitter.com/widgets.js'
+					charset='utf-8'></script>
+				
+			</div>
+		);
+	}
 }
- 
+
 export default Footer;
